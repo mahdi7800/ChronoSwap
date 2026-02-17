3a2b1ec80717e55fd316aae30dd0242f15c07147
mahdi <mdavoudi47@gmail.com>
2026-02-17 23:59:36 +0330

    Add files via upload

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.minute = new System.Windows.Forms.Label();
+            this.sec = new System.Windows.Forms.Label();
+            this.hour = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(66, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(159, 202);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(66, 248);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 38);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Swap";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(159, 248);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 38);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Time";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(252, 248);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 38);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Zero";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button6.Location = new System.Drawing.Point(159, 292);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 44);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Exit";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.minute);
+            this.groupBox1.Controls.Add(this.sec);
+            this.groupBox1.Controls.Add(this.hour);
+            this.groupBox1.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.groupBox1.Location = new System.Drawing.Point(66, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(261, 93);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(148, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = ":";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(74, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 25);
+            this.label1.TabIndex = 9;
+            this.label1.Text = ":";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // minute
+            // 
+            this.minute.AutoEllipsis = true;
+            this.minute.AutoSize = true;
+            this.minute.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.minute.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minute.Location = new System.Drawing.Point(92, 40);
+            this.minute.Name = "minute";
+            this.minute.Size = new System.Drawing.Size(50, 31);
+            this.minute.TabIndex = 10;
+            this.minute.Text = "     ";
+            this.minute.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.minute.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // sec
+            // 
+            this.sec.AutoEllipsis = true;
+            this.sec.AutoSize = true;
+            this.sec.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.sec.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sec.Location = new System.Drawing.Point(172, 40);
+            this.sec.Name = "sec";
+            this.sec.Size = new System.Drawing.Size(50, 31);
+            this.sec.TabIndex = 10;
+            this.sec.Text = "     ";
+            this.sec.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // hour
+            // 
+            this.hour.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.hour.AutoEllipsis = true;
+            this.hour.AutoSize = true;
+            this.hour.BackColor = System.Drawing.SystemColors.Control;
+            this.hour.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.hour.Cursor = System.Windows.Forms.Cursors.PanWest;
+            this.hour.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hour.Location = new System.Drawing.Point(18, 37);
+            this.hour.Name = "hour";
+            this.hour.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.hour.Size = new System.Drawing.Size(56, 37);
+            this.hour.TabIndex = 10;
+            this.hour.Tag = "    ";
+            this.hour.Text = "       ";
+            this.hour.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.hour.UseCompatibleTextRendering = true;
+            this.hour.UseMnemonic = false;
+            this.hour.Click += new System.EventHandler(this.hour_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(66, 101);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(261, 84);
+            this.listBox1.TabIndex = 11;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(252, 202);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 38);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "Lap";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(389, 344);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chronometer";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label minute;
+        private System.Windows.Forms.Label sec;
+        private System.Windows.Forms.Label hour;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void hour_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private int intS = 0;
+        private int intM = 0;
+        private int intH = 0;
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+   
+        private void button5_Click(object sender, EventArgs e)
+        {
+       
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+             intS = 0;
+             intM = 0;
+             intH = 0;
+            hour.Text = "00";
+            minute.Text = "00";
+            sec.Text = "00";
+            button4.Enabled = true ;
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            listBox1.Items.Clear();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            hour.Text = DateTime.Now.Hour.ToString();
+            minute.Text = DateTime.Now.Minute.ToString();
+            sec.Text = DateTime.Now.Second.ToString();
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            hour.Text = DateTime.Now.Hour.ToString();
+            minute.Text = DateTime.Now.Minute.ToString();
+            sec.Text = DateTime.Now.Second.ToString();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            timer2.Enabled = true;
+
+            intS++;
+
+            sec.Text = intS.ToString();
+            minute.Text = intM.ToString();
+            hour.Text = intH.ToString();
+            if (intS == 60)
+            {
+                intS = 0;
+               
+                intM++;
+            }
+            if (intM == 60)
+            {
+                intM = 0;
+                intH++;
+            }
+
+            button4.Enabled = false;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer2.Enabled = false;
+        }
+
+
+        bool isClock = true;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            isClock = !isClock;
+
+            if (isClock)
+            {
+                timer2.Stop();  
+                timer1.Start();  
+            }
+            else
+            {
+                timer1.Stop();  
+                timer2.Start();  
+            }
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            intS++;
+
+            sec.Text = intS.ToString();
+            minute.Text = intM.ToString();
+            hour.Text = intH.ToString();
+            if (intS == 60)
+            {
+                intS = 0;
+
+                intM++;
+            }
+            if (intM == 60)
+            {
+                intM = 0;
+                intH++;
+            }
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        int lapCount = 0; 
+        private void button7_Click(object sender, EventArgs e)
+        {
+            lapCount++;
+          
+            listBox1.Items.Add("Lap : " + lapCount + " - " + intH +" : " + intM + " : " + intS);
+
+        }
+    }
+}
